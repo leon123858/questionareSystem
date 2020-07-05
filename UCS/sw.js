3973f51e05c330f2d1a40420463912c8cec87bcc
leon123858 <a0970785699@gmail.com>
2020-07-06 01:48:49 +0800

    ok

---
+++ UCS/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/WebPage1.html',
+	   '/WebPage2.html',
+       '/index.js',
+	   '/jquery.cookie.js',
+	   '/jquery.js',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
