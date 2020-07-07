de3d37b291b887e17573382b3050b51c889ee674
leon123858 <a0970785699@gmail.com>
2020-07-07 13:37:50 +0800

    asda

--- UCS/sw.js
+++ UCS/sw.js
@@ -2,12 +2,19 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/WebPage1.html',
-	   '/WebPage2.html',
-       '/index.js',
-	   '/jquery.cookie.js',
-	   '/jquery.js',
+       'https://tomatoclock.000webhostapp.com/questionary/',
+       'https://tomatoclock.000webhostapp.com/questionary/confirm.html',
+	   'https://tomatoclock.000webhostapp.com/questionary/memory.html',
+	   'https://tomatoclock.000webhostapp.com/questionary/others.html',
+	   'https://tomatoclock.000webhostapp.com/questionary/show.html',
+	   'https://tomatoclock.000webhostapp.com/questionary/page0.html',
+	   'https://tomatoclock.000webhostapp.com/questionary/page1.html',
+	   'https://tomatoclock.000webhostapp.com/questionary/page2.html',
+       'https://tomatoclock.000webhostapp.com/questionary/index.js',
+	   'https://tomatoclock.000webhostapp.com/questionary/jquery.js',
+	   'https://tomatoclock.000webhostapp.com/questionary/jquery.cookie.js',
+	   'https://tomatoclock.000webhostapp.com/questionary/func.js',
+	   'https://tomatoclock.000webhostapp.com/questionary/style.css',
      ]);
    })
  );
