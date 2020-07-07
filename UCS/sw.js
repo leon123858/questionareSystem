0440a96ed348b9dde876ab710f9f7bf9a6256220
leon123858 <a0970785699@gmail.com>
2020-07-07 14:43:19 +0800

    123

--- UCS/sw.js
+++ UCS/sw.js
@@ -1,10 +1,6 @@
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
-	   if (response) {
-          return response;     // if valid response is found in cache return it
-        } 
-		else{
      return cache.addAll([
        '/questionareSystem/UCS/',
        '/questionareSystem/UCS/confirm.html',
@@ -20,16 +16,32 @@ self.addEventListener('install', function(e) {
 	   '/questionareSystem/UCS/func.js',
 	   '/questionareSystem/UCS/style.css'
      ]);
-		}
    })
  );
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  return cache.match('/confirm.html');
+                });
+            });
+        }
+      })
   );
-});
+});          
