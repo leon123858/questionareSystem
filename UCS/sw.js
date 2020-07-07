6bee72cc895bcee830fd40400ab146040865e089
leon123858 <a0970785699@gmail.com>
2020-07-07 14:47:45 +0800

    qqq

--- UCS/sw.js
+++ UCS/sw.js
@@ -2,19 +2,19 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/questionareSystem/UCS/',
-       '/questionareSystem/UCS/confirm.html',
-	   '/questionareSystem/UCS/memory.html',
-	   '/questionareSystem/UCS/others.html',
-	   '/questionareSystem/UCS/show.html',
-	   '/questionareSystem/UCS/page0.html',
-	   '/questionareSystem/UCS/page1.html',
-	   '/questionareSystem/UCS/page2.html',
-       '/questionareSystem/UCS/index.js',
-	   '/questionareSystem/UCS/jquery.js',
-	   '/questionareSystem/UCS/jquery.cookie.js',
-	   '/questionareSystem/UCS/func.js',
-	   '/questionareSystem/UCS/style.css'
+       './questionareSystem/UCS/',
+       './questionareSystem/UCS/confirm.html',
+	   './questionareSystem/UCS/memory.html',
+	   './questionareSystem/UCS/others.html',
+	   './questionareSystem/UCS/show.html',
+	   './questionareSystem/UCS/page0.html',
+	   './questionareSystem/UCS/page1.html',
+	   './questionareSystem/UCS/page2.html',
+       './questionareSystem/UCS/index.js',
+	   './questionareSystem/UCS/jquery.js',
+	   './questionareSystem/UCS/jquery.cookie.js',
+	   './questionareSystem/UCS/func.js',
+	   './questionareSystem/UCS/style.css'
      ]);
    })
  );
