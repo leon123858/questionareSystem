fee8011b00f7a747c0bfd4179704fabc8fcee3cc
leon123858 <a0970785699@gmail.com>
2020-07-07 13:48:13 +0800

    pwa test 3 in github

--- UCS/index.js
+++ UCS/index.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/sw.js')
+           .register('/questionareSystem/UCS/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
